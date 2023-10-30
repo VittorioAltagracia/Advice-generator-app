5b99c828873b742e26deb80413e60bf14179e33a
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-10-30 17:59:50 -0500

    completed Advice generator challenge - finished layout, added comments and done all the styling

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ### Advice generator app
 
 Makes a call to a public api and retrieves a random advice
+It is fully responsive
 
 This challenge is found through Front End Mentor platform to practice my skills.
--- getData.js
+++ getData.js
@@ -4,16 +4,17 @@ const dice = document.querySelector(".dice");
 const adviceParagraph = document.querySelector(".advice");
 const adviceNum = document.querySelector(".advice-num");
 
-//  function that fetches advices
-async function getAdvice() {
+//  function that fetches advices and updates html elements
+async function getAdviceAndDisplayIt() {
   try {
     const randomAdvice = await fetch("https://api.adviceslip.com/advice");
+    // line 12 - parcing data into json
     const returnedAdvice = await randomAdvice.json();
     const { slip } = returnedAdvice;
-    console.log(slip);
-    adviceNum.innerHTML = slip.id;
-    adviceParagraph.innerHTML = slip.advice;
-    return slip;
+
+    // below - updating html elements
+    adviceNum.innerHTML = `Advice #${slip.id}`;
+    adviceParagraph.innerHTML = `❝${slip.advice}❞`;
   } catch (error) {
     console.log(error);
   }
@@ -21,6 +22,11 @@ async function getAdvice() {
 
 // onclick we execute getAdvice
 dice.addEventListener("click", () => {
-  getAdvice();
-  console.log("ok - it worked");
+  getAdviceAndDisplayIt();
+});
+
+// when a page loads initially we make an api call right away
+
+window.addEventListener("load", () => {
+  getAdviceAndDisplayIt();
 });
